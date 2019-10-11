b49ba01dd3399e32459744b64b7297989b860722
Frederick Articuno <mckiyoshi@gmail.com>
2019-10-10 23:09:03 -0400

    First LIRI commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# LIRI
+1. Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
+2. Give a high-level overview of how the app is organized
+3. Give start-to-finish instructions on how to run the app
+4. Include screenshots, gifs or videos of the app functioning
+5. Contain a link to a deployed version of the app
+6. Clearly list the technologies used in the app
+7. State your role in the app development
+
+1. This app is intened to search using LIRI which is a Language Interpretation and Recognition Interface.
+2. The app is run via CLI
+3. 
+4.
+5.
+6. Tech involved
+    * [Visual-Studio-Code](https://code.visualstudio.com/)
+    * [NodeJS](https://nodejs.org/en/docs/)
+    * [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+    * [Axios](https://www.npmjs.com/package/axios)
+    * [OMDB API](http://www.omdbapi.com)
+    * [Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
+    * [Moment](https://www.npmjs.com/package/moment)
+    * [DotEnv](https://www.npmjs.com/package/dotenv)
+    
+7. I am the primary developer for this app.
---
+++ index.js
@@ -0,0 +1,73 @@
+var inquirer = require('inquirer') 
+var spotify = require('spotify');
+var axios = require("axios");
+
+inquirer
+.prompt([
+    {
+      type: 'list',
+      message: "What are you looking for?",
+      choices: ['music','movies','Bands In Town'],
+      name:"Search1",
+    }
+]).then (function(inquirerResponse) {
+    console.log(inquirerResponse)
+    if (inquirerResponse.Search1 =='music') {
+        console.log('music')
+        inquirer
+            .prompt([
+                {
+                type: 'input',
+                message: "What music are you looking for?",
+                name:"SearchMusic",
+                }
+            ]).then (function(inquirerResponse) { 
+                console.log("Looking for",inquirerResponse.SearchMusic,"music...")
+                spotify.search({ type: 'track', query: inquirerResponse.SearchMusic}, function(err, data) {
+                    if ( err ) {
+                        console.log('Error occurred: ' + err);
+                        return;
+                    }
+                    console.log(data)
+                });
+            }
+        )}
+//===================================================================================================================
+    if (inquirerResponse.Search1 =='movies') {
+        console.log('movies')
+        inquirer
+            .prompt([
+                {
+                type: 'input',
+                message: "What movie are you looking for?",
+                name:"SearchMovies",
+                }
+            ]).then (function(inquirerResponse) { 
+                console.log("Looking for",inquirerResponse.SearchMovies,"Movie...")
+                axios.get("http://www.omdbapi.com/?t="+ inquirerResponse.SearchMovies +"&y=&plot=short&apikey=trilogy").then(
+                    function(response) {
+                        console.log("The movie's info is: " + JSON.stringify(response.data));
+                    }
+                );
+            }
+        )}    
+//===================================================================================================================
+    if (inquirerResponse.Search1 =='Bands In Town') {
+        console.log('Bands In Town')
+        inquirer
+            .prompt([
+                {
+                type: 'input',
+                message: "What Bands are you looking for?",
+                name:"SearchBandsInTown",
+                }
+            ]).then (function(inquirerResponse) { 
+                console.log("Looking for",inquirerResponse.SearchBandsInTown,"Bands in town...")
+                axios.get("http://www.bandsintown.com/event/13722599?app_id=foo&artist=Skrillex&came_from=67").then(
+                    function(response) {
+                        console.log("The band's info is: " + JSON.stringify(response.data));
+                    }
+                );
+            }
+        )}
+});
